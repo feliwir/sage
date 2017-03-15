e488412d1cde38ec78588edf06e7e4c7a0936fc7
Stephan Vedder <stephan.vedder@gmail.com>
2017-03-15 17:13:55 +0100

    Fix YUV to RGB conversion

--- vp6/PixelFormat.cs
+++ vp6/PixelFormat.cs
@@ -6,84 +6,32 @@ namespace sage.vp6
 {
     class PixelFormat
     {
-        public static byte[] Yuv420pToRGB(byte[] y,byte[] u, byte[] v,uint width,uint height)
+        public static byte[] Yuv420pToRGB(byte[] ybuf, byte[] ubuf, byte[] vbuf, uint width, uint height)
         {
             uint size = width * height;
             //Allocate the RGB buffer
             byte[] buffer = new byte[size * 3];
 
-            int uv_index = 0, pass = 0;
-            int b, g, r;
-            int rgb_index = 0;
-            for (int px=0; px<size/6;++px)
+            for (int row = 0; row < height; ++row)
             {
-                if (pass == 2)
+                for (int col = 0; col < width; ++col)
                 {
-                    pass = 0;
-                    uv_index += 3;
+                    //get YUV values
+                    ref byte y = ref ybuf[row * width + col];
+                    ref byte u = ref ubuf[(row / 2) * (width / 2) + col / 2];
+                    ref byte v = ref vbuf[(row / 2) * (width / 2) + col / 2];
+
+                    //get RGB values
+                    ref byte r = ref buffer[(row * width + col) * 3];
+                    ref byte g = ref buffer[(row * width + col) * 3 + 1];
+                    ref byte b = ref buffer[(row * width + col) * 3 + 2];
+
+                    //perform the conversion
+                    b = (byte)(1.164*(y - 16) + 2.018*(u - 128));
+                    g = (byte)(1.164 *(y - 16) - 0.813*(v - 128) - 0.391*(u - 128));
+                    r = (byte)(1.164 *(y - 16) + 1.596*(v - 128));
                 }
-
-                int y1 = y[px];
-                int y2 = y[px + 1];
-                int y3 = y[px + 2];
-                int y4 = y[px + 3];
-                int y5 = y[px + 4];
-                int y6 = y[px + 5];
-
-                int u1 = u[uv_index];
-                int u2 = u[uv_index + 1];
-                int u3 = u[uv_index + 2];
-
-                int v1 = v[uv_index];
-                int v2 = v[uv_index + 1];
-                int v3 = v[uv_index + 2];
-                
-                //1
-                r = (int)(1.164 * (y1 - 16) + 1.596 * (v1 - 128));
-                g = (int)(1.164 * (y1 - 16) - 0.813 * (v1 - 128) - 0.391 * (u1 - 128));
-                b = (int)(1.164 * (y1 - 16) + 2.018 * (u1 - 128));
-                buffer[rgb_index++] = Util.ClipByte(r);
-                buffer[rgb_index++] = Util.ClipByte(g);
-                buffer[rgb_index++] = Util.ClipByte(b);
-                //2
-                r = (int)(1.164 * (y2 - 16) + 1.596 * (v1 - 128));
-                g = (int)(1.164 * (y2 - 16) - 0.813 * (v1 - 128) - 0.391 * (u1 - 128));
-                b = (int)(1.164 * (y2 - 16) + 2.018 * (u1 - 128));
-                buffer[rgb_index++] = Util.ClipByte(r);
-                buffer[rgb_index++] = Util.ClipByte(g);
-                buffer[rgb_index++] = Util.ClipByte(b);
-                //3
-                r = (int)(1.164 * (y3 - 16) + 1.596 * (v2 - 128));
-                g = (int)(1.164 * (y3 - 16) - 0.813 * (v2 - 128) - 0.391 * (u2 - 128));
-                b = (int)(1.164 * (y3 - 16) + 2.018 * (u2 - 128));
-                buffer[rgb_index++] = Util.ClipByte(r);
-                buffer[rgb_index++] = Util.ClipByte(g);
-                buffer[rgb_index++] = Util.ClipByte(b);
-                //4
-                r = (int)(1.164 * (y4 - 16) + 1.596 * (v2 - 128));
-                g = (int)(1.164 * (y4 - 16) - 0.813 * (v2 - 128) - 0.391 * (u2 - 128));
-                b = (int)(1.164 * (y4 - 16) + 2.018 * (u2 - 128));
-                buffer[rgb_index++] = Util.ClipByte(r);
-                buffer[rgb_index++] = Util.ClipByte(g);
-                buffer[rgb_index++] = Util.ClipByte(b);
-                //5
-                r = (int)(1.164 * (y5 - 16) + 1.596 * (v3 - 128));
-                g = (int)(1.164 * (y5 - 16) - 0.813 * (v3 - 128) - 0.391 * (u3 - 128));
-                b = (int)(1.164 * (y5 - 16) + 2.018 * (u3 - 128));
-                buffer[rgb_index++] = Util.ClipByte(r);
-                buffer[rgb_index++] = Util.ClipByte(g);
-                buffer[rgb_index++] = Util.ClipByte(b);
-                //6
-                r = (int)(1.164 * (y6 - 16) + 1.596 * (v3 - 128));
-                g = (int)(1.164 * (y6 - 16) - 0.813 * (v3 - 128) - 0.391 * (u3 - 128));
-                b = (int)(1.164 * (y6 - 16) + 2.018 * (u3 - 128));
-                buffer[rgb_index++] = Util.ClipByte(r);
-                buffer[rgb_index++] = Util.ClipByte(g);
-                buffer[rgb_index++] = Util.ClipByte(b);
-
-                pass++;
             }
-
             return buffer;
         }
 
